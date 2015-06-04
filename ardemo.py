b5008981a0f4dc44902f9b54716560e967a9c934
Matt Switlik <switlik@oakland.edu>
2015-06-04 16:56:06 -0400

    open and display gif

--- ardemo.py
+++ ardemo.py
@@ -1,19 +1,24 @@
 import numpy as np
 import cv, cv2
 import zbar
+from PIL import Image
 
 cap = cv2.VideoCapture(0)
 
-print cap.get(cv.CV_CAP_PROP_FRAME_WIDTH),cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+#print cap.get(cv.CV_CAP_PROP_FRAME_WIDTH),cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
 cap.set(cv.CV_CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 720)
-print cap.get(cv.CV_CAP_PROP_FRAME_WIDTH),cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+#print cap.get(cv.CV_CAP_PROP_FRAME_WIDTH),cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
 
 cv2.namedWindow('AR_Demo', cv2.WINDOW_NORMAL)
 
 scanner = zbar.ImageScanner()
 scanner.set_config(0, zbar.Config.ENABLE, 0) #disable all symbols
 scanner.set_config(zbar.Symbol.QRCODE, zbar.Config.ENABLE, 1) #enable QR codes
+
+gifimg = Image.open("gifs/surprise-kitten.gif")
+gifframe = np.array(gifimg)
+gifframeindex = 0
 
 print
 print "Q to exit."
@@ -24,9 +29,17 @@ while(True):
     ret, frame = cap.read()
 
     height, width, depth = frame.shape
-    
+
+    try:
+        gifimg.seek(gifframeindex)
+        gifframe = np.array(gifimg)
+        gifframeindex += 1
+    except EOFError:
+        gifframeindex = 0
+        gifimg.seek(gifframeindex)
+        gifframe = np.array(gifimg)
+        
     # Our operations on the frame come here
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     raw = frame.tostring()
     image = zbar.Image(width, height, 'Y800', raw)
     scanner.scan(image)
@@ -34,7 +47,7 @@ while(True):
         print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
         
     # Display the resulting frame
-    cv2.imshow('AR_Demo', frame)
+    cv2.imshow('AR_Demo', gifframe)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
